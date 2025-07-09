5e4c7192a94750e8c3670692ea40332b48cba01d
telegrambot84 <chiragbadgujar84@gmail.com>
2025-07-09 22:44:29 +0530

    Create config.py

---
+++ mfinder/config.py
@@ -0,0 +1,7 @@
+import os
+
+APP_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+YOUR_CHANNEL_ID = int(os.getenv("YOUR_CHANNEL_ID"))
