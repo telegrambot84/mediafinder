c22a98bfab228640bb22c61bda2c601978ace149
telegrambot84 <chiragbadgujar84@gmail.com>
2025-07-10 18:39:39 +0530

    Update __init__.py

--- mfinder/__init__.py
+++ mfinder/__init__.py
@@ -15,7 +15,7 @@ APP_ID = os.environ.get("APP_ID", "")
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 DB_URL = os.environ.get("DB_URL", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 ADMINS = [
     int(user) if id_pattern.search(user) else user
     for user in os.environ.get("ADMINS", "").split()
